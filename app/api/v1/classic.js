f145f6d687eba9c44c686febafc0bb43c2658ec1
Meng YuCheng <maxmeng93@gmail.com>
2019-08-15 16:46:26 +0800

    update

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/v1/:id/book/latest', (ctx, next) => {
+router.get('/v1/classic/latest', (ctx, next) => {
   ctx.body = {
     key: 'classic'
   };
+  throw new Error('api exception');
 });
 
 module.exports = router;
